88ed8b504f3e86352f5b14f533ad52374192b776
burkeholland <burkeholland@gmail.com>
2023-09-24 10:47:01 -0500

    working on chat history

---
+++ Client/StateContainer.cs
@@ -0,0 +1,59 @@
+using RudeGPT.Shared;
+using System.Text.Json;
+using Microsoft.JSInterop;
+
+public class StateContainer
+{
+    private ChatSession chatSession = new ChatSession();
+
+    private List<ChatSession> chatHistory = new List<ChatSession>();
+
+    private IJSRuntime _jsRuntime;
+
+    public StateContainer(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+
+    public ChatSession ChatSession
+    {
+        get => chatSession;
+        set
+        {
+            chatSession = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public List<ChatSession> ChatHistory
+    {
+        get => chatHistory;
+        set
+        {
+            Console.WriteLine("Setting chat history");
+            chatHistory = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public void SaveHistoryToLocalStorage()
+    {
+        var json = JsonSerializer.Serialize(ChatHistory);
+        _jsRuntime.InvokeVoidAsync("localStorage.setItem", "chatHistory", json);
+    }
+
+    public async Task LoadHistoryFromLocalStorage()
+    {
+        var json = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "chatHistory");
+        if (!string.IsNullOrEmpty(json))
+        {
+            var chatHistory = JsonSerializer.Deserialize<List<ChatSession>>(json);
+            ChatHistory = chatHistory ?? new List<ChatSession>();
+        }
+    }
+
+    public event Action? OnChange;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
