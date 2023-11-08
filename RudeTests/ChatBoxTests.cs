98e78374fb7a9d7cb9e3342dee8da6e669dac24d
Leslie Richardson <lerich@microsoft.com>
2023-11-08 13:42:30 -0800

    added testing example for dev kit demo

---
+++ RudeTests/ChatBoxTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using BlazorApp.Client.Shared;
+
+namespace RudeTests;
+
+[TestClass]
+public class ChatBoxTests : BunitTestContext
+{
+    [TestMethod]
+	public void ModelIsEmptyAtInitialization()
+	{
+        var cut = RenderComponent<ChatBox>();
+        
+		// Arrange
+		Assert.AreEqual("", cut.Instance.Model);
+	}
+
+    [TestMethod]
+	public void HandleSendButtonClickSuccess()
+	{
+        var cut = RenderComponent<ChatBox>();
+        cut.Find("textarea").Input("Hello World");
+
+        Assert.AreEqual("Hello World", cut.Instance.Model);       
+		cut.Find("button").Click();
+        Assert.AreEqual("", cut.Instance.Model); 
+
+        //string textarea = "<textarea class=\"textarea\" @bind=\"\" \u0040onkeydown:preventDefault=\"true\" \u0040bind:event=\"oninput\" \u0040onkeydown=\"HandleKeyDown\"></textarea>";
+        //Debug.WriteLine(textarea);
+        //cut.Find("textarea").MarkupMatches("<textarea class=\"textarea\" @bind=\"\" \u0040onkeydown:preventDefault=\"true\" \u0040bind:event=\"oninput\" \u0040onkeydown=\"HandleKeyDown\"></textarea>");     
+	}
+}
