e200404b3da7b564fc9758c47cde90147b99dcbe
Your Name <youremail@example.com>
2023-11-12 11:23:08 -0800

    Fixing the namespace

--- RudeTests/ChatBoxTests.cs
+++ RudeTests/ChatBoxTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using BlazorApp.Client.Shared;
+using RudeGPT.Client.Shared;
 
 namespace RudeTests;
 
@@ -8,28 +8,29 @@ namespace RudeTests;
 public class ChatBoxTests : BunitTestContext
 {
     [TestMethod]
-	public void ModelIsEmptyAtInitialization()
-	{
+    public void ModelIsEmptyAtInitialization()
+    {
         var cut = RenderComponent<ChatBox>();
-        
-		// Arrange
-		Assert.AreEqual("", cut.Instance.Model);
-	}
+
+        // Arrange
+        Assert.AreEqual("", cut.Instance.Model);
+    }
 
     [TestMethod]
-	public void HandleSendButtonClickSuccess()
-	{
+    public void HandleSendButtonClickSuccess()
+    {
         var cut = RenderComponent<ChatBox>();
         cut.Find("textarea").Input("Hello World");
 
-        Assert.AreEqual("Hello World", cut.Instance.Model);       
-		cut.Find("button").Click();
+        Assert.AreEqual("Hello World", cut.Instance.Model);
+        cut.Find("button").Click();
         Assert.AreEqual("", cut.Instance.Model);
-	}
+    }
 
     [TestMethod]
-    public void SendButtonRenderedCorrectly() {
+    public void SendButtonRenderedCorrectly()
+    {
         var cut = RenderComponent<ChatBox>();
-        cut.Find("button").MarkupMatches(@"<button class=""button is-primary is-block mr-3 is-rounded""  >Send</button>");
+        cut.Find("button").MarkupMatches(@"<button class=""button is-primary is-block mr-3 is-rounded""  >Sned</button>");
     }
 }
